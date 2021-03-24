3dd9a48fb0f605950082fb87dcc2efd3d20d9387
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-23 20:51:14 -0500

    color elements updated in tetros

--- tetris/src/StagePieces/tetrominos.js
+++ tetris/src/StagePieces/tetrominos.js
@@ -1,6 +1,6 @@
 //shapes
 export const TETROMINOS = {
-  0: { shape: [[0]], color: 'add color here'}, //EMPTY SPACE
+  0: { shape: [[0]], color: '0,0,0'}, //EMPTY SPACE
   I: { 
     shape: [
             [0, 'I', 0, 0],
@@ -8,27 +8,27 @@ export const TETROMINOS = {
             [0, 'I', 0, 0],
             [0, 'I', 0, 0]
           ], 
-    color: 'rgb(0, 0, 255)'},
+    color: '0, 0, 255'},
   J: { 
     shape: [
             [0,'J',0],
             [0,'J',0],
             ['J','J',0]
           ], 
-    color: 'rgb(0, 255, 0)'},
+    color: '0, 255, 0'},
   L: { 
     shape: [
             [0,'L',0],
             [0,'L',0],
             [0,'L','L']
           ], 
-    color: 'rgb(255, 255, 0)'},
+    color: '255, 255, 0'},
   O: { 
     shape: [
             ['O', 'O'],
             ['O', 'O'],
           ], 
-    color: 'rgb(255, 128, 0)'},
+    color: '255, 128, 0'},
 
   S: { 
     shape: [
@@ -36,21 +36,21 @@ export const TETROMINOS = {
             ['S','S',0],
             [0,0,0]
           ], 
-    color: 'rgb(255, 0, 0)'},
+    color: '255, 0, 0'},
   T: { 
     shape: [
             [0,0,0],
             ['T','T','T'],
             [0,'T',0]    
           ], 
-    color: 'rgb(207, 7, 255)'},
+    color: '207, 7, 255'},
   Z: { 
     shape: [
             ['Z','Z',0],
             [0,'Z','Z'],
             [0,0,0]
           ], 
-    color: 'rgb(140, 92, 242)'},
+    color: '140, 92, 242'},
 }
 
 //gen shapes
