9254411cf1a1f0fc5aa59bf9336061a6b3757c12
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-23 20:47:46 -0500

    found severl typos in game helper

--- tetris/src/StagePieces/gameHelper.js
+++ tetris/src/StagePieces/gameHelper.js
@@ -6,11 +6,11 @@ export const createStage = () => Array.from(Array(STAGE_HEIGHT), () => Array(STA
 
 export const checkCollision = (player, stage, {x: moveX, y:moveY}) => {
 
-  for(let y=0; y < player.tetrminos.length; y++) {
+  for(let y=0; y < player.tetrominos.length; ++y) {
     
-    for(let x=0; x < player.tetrminos.length; x++) {
+    for(let x=0; x < player.tetrominos.length; ++x) {
       //make sure we are in the tetromino cell 
-      if(player.tetrminos[y][x] !==0) {
+      if(player.tetrominos[y][x] !==0) {
         //make sure we are within the game area, both height and width nor drop out the bottom
         //set the section variable to verify the if collided and set the clear to merge
 
@@ -19,11 +19,11 @@ export const checkCollision = (player, stage, {x: moveX, y:moveY}) => {
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
           stage[y + player.pos.y + moveY][y + player.pos.x + moveX][1] !== 'clear' 
         ) {
-          return true
+          return true;
         }
       }
 
     }
   }
-
+  return false;
 }
