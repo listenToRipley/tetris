d8409fd73e13f887001ec77ebcf24ee0143c5abd
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-23 20:39:54 -0500

    game hook messed up

--- tetris/src/Hooks/useGameStatus.js
+++ tetris/src/Hooks/useGameStatus.js
@@ -6,9 +6,10 @@ export const useGameStatus = rowsCleared => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
+  const linePoints = [40, 100, 300, 1200]
+
   const calcScore = useCallback(() => {
       //if you score? 
-      const linePoints = [40, 100, 300, 1200]
 
       if(rowsCleared > 0) {
         setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1))
