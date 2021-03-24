cf27786b197001ce8fc3a479d98ce41b3559c76e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-23 21:21:37 -0500

    change linepoints to usememo

--- tetris/src/Hooks/useGameStatus.js
+++ tetris/src/Hooks/useGameStatus.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useCallback} from 'react';
+import {useState, useEffect, useCallback, useMemo} from 'react';
 
 export const useGameStatus = rowsCleared => {
 
@@ -6,7 +6,8 @@ export const useGameStatus = rowsCleared => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200]
+  let linePoints = useMemo(() => {}, []);
+  linePoints = [40, 100, 300, 1200];
 
   const calcScore = useCallback(() => {
       //if you score? 
