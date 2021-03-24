74e4e7c0c2fafe5543a3a0f81e4b9f215e57b30a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-23 20:27:33 -0500

    update line points

--- tetris/src/Hooks/useGameStatus.js
+++ tetris/src/Hooks/useGameStatus.js
@@ -6,11 +6,9 @@ export const useGameStatus = rowsCleared => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-
-  const linePoints = [40, 100, 300, 1200]
-
   const calcScore = useCallback(() => {
       //if you score? 
+      const linePoints = [40, 100, 300, 1200]
 
       if(rowsCleared > 0) {
         setScore(prev => prev + linePoints[rowsCleared - 1] * (setLevel + 1))
