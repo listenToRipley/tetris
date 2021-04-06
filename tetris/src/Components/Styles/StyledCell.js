af6a9683a7c79ddcb6be7192856c6e5615d393b5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-05 21:36:12 -0500

    see if rendering is actually a style issue

--- tetris/src/Components/Styles/StyledCell.js
+++ tetris/src/Components/Styles/StyledCell.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 export const StyledCell = styled.div`
-  width: 5px auto;
+  width: auto;
   background: #000000;
-  color:rgb(164, 164, 184);
+  ${'' /* color:rgb(164, 164, 184); */}
   border: ${props => props.type === 0 ? '0px solid' : '1px solid'};
   border-bottom-color: rgba(${props => props.color}, 0.1);
   border-right-color: rgba(${props => props.color}, 1);
   border-left-color: rgba(${props => props.color}, 0.3);
   border-top-color: rgba(${props => props.color}, 1);
-  margin: 0;
-  padding:0;
+  ${'' /* margin: 0;
+  padding:0; */}
 `
