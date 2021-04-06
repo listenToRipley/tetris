864e82e8e0871c6be4c210882c675bf468e86791
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-05 21:46:09 -0500

    style looks okay

--- tetris/src/Components/Styles/StyledTetris.js
+++ tetris/src/Components/Styles/StyledTetris.js
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 export const StyledTetrisWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background: url('https://images.unsplash.com/photo-1573689705887-bc0763c82ea2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80');
+  background: url('https://images.unsplash.com/photo-1573689705887-bc0763c82ea2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80') #000;
   background-size: cover;
   overflow: hidden;
 `
 
 export const StyledTetris = styled.div`
-  display: flexbox;
-  align-items: flex-start;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   padding: 40px;
   margin: 0;
   max-width: 50%;
@@ -20,6 +21,6 @@ export const StyledTetris = styled.div`
       max-width: 200px;
       display: block;
       padding: 0 20px;
-      margin-left: 5vw
+      ${'' /* margin-left: 5vw */}
     }
   `
