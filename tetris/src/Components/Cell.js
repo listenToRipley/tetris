776f243704500d25a261125728a859d4e9ed3f9f
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-05 21:56:05 -0500

    do not think the cell is getting removed

--- tetris/src/Components/Cell.js
+++ tetris/src/Components/Cell.js
@@ -3,7 +3,10 @@ import {StyledCell} from './Styles/StyledCell'
 import {TETROMINOS} from '../tetrominos'
 
 const Cell = ({type}) => (
-  <StyledCell type={type} color={TETROMINOS[type].color} />
+
+  <StyledCell type={type} color={TETROMINOS[type].color} >
+  {console.log('rerender in Cell')}
+  </StyledCell>
 )
 //only render as needed
 export default React.memo(Cell);
