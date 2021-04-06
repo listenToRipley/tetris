15c1d4ff724312e25beabdfff6a055703321790f
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-05 21:39:43 -0500

    still a little better, but seems like stage is rendering over shapes, not shapes over stage

--- tetris/src/Components/Styles/StyledStage.js
+++ tetris/src/Components/Styles/StyledStage.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 export const StyledStage = styled.div`
   display: grid;
-  grid-area: auto;
+  ${'' /* grid-area: auto; */}
   grid-template-row: repeat(${props => props.height}, cal(25vw / ${props => props.width}));
   grid-template-columns: repeat(${props => props.width}, 1fr);
-  grid-gap: .05px;
+  grid-gap: 1px;
   border: 2px solid #333;
   width:100%;
   max-width: 45vw;
-  max-height: 100vh
+  max-height: 100vh;
   background: #111; 
 `
 
