8efa9c0fd6760ec7a8f18b15ef5251f57c2db0a2
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-11 18:31:46 -0500

    typo for teros to singular

--- tetris/src/gameHelper.js
+++ tetris/src/gameHelper.js
@@ -7,11 +7,11 @@ console.table('re-render : ',createStage())
 
 export const checkCollision = (player, stage, {x: moveX, y:moveY}) => {
 
-  for(let y=0; y < player.tetrominos.length; ++y) {
+  for(let y=0; y < player.tetromino.length; ++y) {
     
-    for(let x=0; x < player.tetrominos.length; ++x) {
+    for(let x=0; x < player.tetromino.length; ++x) {
       //make sure we are in the tetromino cell 
-      if(player.tetrominos[y][x] !==0) {
+      if(player.tetromino[y][x] !==0) {
         //make sure we are within the game area, both height and width nor drop out the bottom
         //set the section variable to verify the if collided and set the clear to merge
 
