0790435c80382871c97030f6a4ae1d2431b9f64f
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-14 18:46:11 -0500

    it is magic! can now see rows

--- tetris/src/Components/Stage.js
+++ tetris/src/Components/Stage.js
@@ -5,6 +5,7 @@ import {StyledStage} from './Styles/StyledStage'
 export const Stage = ({stage}) => {
   const height = stage.length
   const width = stage[0].length
+  console.log(`why can't you ready width on stage?`, width)
   return(
     <StyledStage width={width} height={height}>
       {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
