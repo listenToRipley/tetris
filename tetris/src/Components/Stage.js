e07d742557d61fc0e6dd36726fe70cc83ff71691
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-14 18:47:10 -0500

    renamed variable to keep with theme

--- tetris/src/Components/Stage.js
+++ tetris/src/Components/Stage.js
@@ -3,11 +3,11 @@ import Cell from './Cell';
 import {StyledStage} from './Styles/StyledStage'
 
 export const Stage = ({stage}) => {
-  const height = stage.length
-  const width = stage[0].length
-  console.log(`why can't you ready width on stage?`, width)
+  const columns = stage.length
+  const rows = stage[0].length
+  console.log(`why can't you ready width on stage?`, rows)
   return(
-    <StyledStage width={width} height={height}>
+    <StyledStage width={rows} height={columns}>
       {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
       {console.log('mapping the stage component')}
     </StyledStage>
