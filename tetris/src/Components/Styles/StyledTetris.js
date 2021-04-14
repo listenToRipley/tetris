7709a2b9c34c76c18b8e393dc28a7a7d553a4da3
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-13 22:48:31 -0500

    background image is wrong

--- tetris/src/Components/Styles/StyledTetris.js
+++ tetris/src/Components/Styles/StyledTetris.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
-import background from '../Img/background.jpg'
+import city from '../Img/city.png'
 
 export const StyledTetrisWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background: url('https://images.unsplash.com/photo-1573689705887-bc0763c82ea2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80') ;
+  background: url(city);
   background-size: cover;
   overflow: hidden;
 `
